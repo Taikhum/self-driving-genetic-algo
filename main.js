a887a4184a9b2890facd0f426ff5660587e518f1
taikhum <taikhumk@gmail.com>
2023-06-20 17:08:19 +0530

    added single lane

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("visualization");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width/2,canvas.width*.9);
 const car = new CarModel(100,100,30,50);
 car.drawCar(ctx);
 
@@ -11,6 +12,7 @@ simulatorLoop();
 function simulatorLoop(){
     car.updateCarState();
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.drawCar(ctx);
     requestAnimationFrame(simulatorLoop);
 
